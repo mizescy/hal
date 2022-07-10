c14119c457fdc1e82d81cc2e3d307172f65a4942
Christopher Sherman <isa_christopher_sherman@sp.edu.sg>
2022-07-10 17:02:06 +0800

    Update HAL keypad driver to configure callback at initialization

--- hal_keypad.py
+++ hal_keypad.py
@@ -11,7 +11,13 @@ MATRIX=[ [1,2,3],
 ROW=[6,20,19,13] #row pins
 COL=[12,5,16] #column pins
 
-def init():
+global cbk_func
+
+def init(key_press_cbk):
+    global cbk_func
+
+    cbk_func = key_press_cbk
+
     #set column pins as outputs, and write default value of 1 to each
     for i in range(3):
         GPIO.setup(COL[i],GPIO.OUT)
@@ -21,7 +27,9 @@ def init():
     for j in range(4):
         GPIO.setup(ROW[j],GPIO.IN,pull_up_down=GPIO.PUD_UP)
 
-def get_key(key_press_cbk):
+def get_key():
+    global cbk_func
+
     #scan keypad
     while (True):
         for i in range(3): #loop thru’ all columns
@@ -29,7 +37,7 @@ def get_key(key_press_cbk):
             for j in range(4): #check which row pin becomes low
                 if GPIO.input(ROW[j])==0: #if a key is pressed
                     #print (MATRIX[j][i]) #print the key pressed
-                    key_press_cbk(MATRIX[j][i])
+                    cbk_func(MATRIX[j][i])
                     #return MATRIX[j][i]
 
                     while GPIO.input(ROW[j])==0: #debounce
